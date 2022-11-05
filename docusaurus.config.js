639e3fdd6cebe0a246fcdd552f5720b6429317d8
davidyoon <qqpo12@naver.com>
2022-11-05 16:07:26 +0900

    Modify the blog logo
    * Modified the blog logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
         title: 'Davidyoon Tech Blog',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/blogLogo.png',
         },
         items: [
           {
