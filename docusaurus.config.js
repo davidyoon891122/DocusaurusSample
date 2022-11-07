1e0cdc5ff34d442c7cf871f82b4a17d5d3b6e1c7
davidyoon <qqpo12@naver.com>
2022-11-07 21:05:03 +0900

    Modify config texts
    * Modified intro to resume

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,8 +89,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Resume',
+                to: '/docs/resume',
               },
             ],
           },
